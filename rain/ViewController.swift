3e6fc78c0f7e7c44473f957a44f369895923aad5
nic <nicholaswillia5@gmail.com>
2016-10-04 07:20:11 -0400

    Initial Commit

---
+++ rain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rain
+//
+//  Created by nic on 4/10/2016.
+//  Copyright © 2016 nicksdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
